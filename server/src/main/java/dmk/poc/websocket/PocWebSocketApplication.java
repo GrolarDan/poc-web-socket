26ab4fe739de47b739de0e7f8f5ca7e298a3233c
Daniel Masek <daniel@masci.cz>
2025-07-25 08:42:22 +0200

    Server implementation

---
+++ server/src/main/java/dmk/poc/websocket/PocWebSocketApplication.java
@@ -0,0 +1,13 @@
+package dmk.poc.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocWebSocketApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/dmk/poc/websocket/PocWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package dmk.poc.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocWebSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
